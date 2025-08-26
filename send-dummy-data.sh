7c2d75e27e122f46a125ad0eae3d883a0a4ae144
Patthanan Bhandhumani <123183496+patthananb@users.noreply.github.com>
2025-08-26 19:48:50 +0700

    feat: randomly insert null values

--- send-dummy-data.sh
+++ send-dummy-data.sh
@@ -24,6 +24,10 @@ SAMPLES=1000
 AMPLITUDE=2000
 OFFSET=2047
 
+# Probability that any given channel value is set to null
+# (e.g. 0.1 = 10% chance)
+NULL_PROBABILITY=0.1
+
 # Function to generate random frequency between 40-80 Hz
 random_freq() {
   awk -v seed=$RANDOM -v min=40 -v max=80 'BEGIN{srand(seed); print int(min+rand()*(max-min+1))}'
@@ -36,6 +40,12 @@ I1_FREQ=$(random_freq)
 I2_FREQ=$(random_freq)
 I3_FREQ=$(random_freq)
 
+# Randomly replace a value with null based on NULL_PROBABILITY
+maybe_null() {
+  local val="$1"
+  awk -v seed="$RANDOM" -v v="$val" -v p="$NULL_PROBABILITY" 'BEGIN{srand(seed); if (rand() < p) printf "null"; else printf v;}'
+}
+
 # Build JSON payload of sine wave samples
 JSON_PAYLOAD="{\"faultType\":\"$FAULT_TYPE\",\"faultLocation\":\"$FAULT_LOCATION\",\"date\":\"$CURRENT_DATE\",\"time\":\"$CURRENT_TIME\",\"data\":["
 for ((n = 0; n < SAMPLES; n++)); do
@@ -46,6 +56,12 @@ for ((n = 0; n < SAMPLES; n++)); do
       int(amp*sin(2*pi*f1*t)+offset), int(amp*sin(2*pi*f2*t)+offset), int(amp*sin(2*pi*f3*t)+offset), \
       int(amp*sin(2*pi*f4*t)+offset), int(amp*sin(2*pi*f5*t)+offset), int(amp*sin(2*pi*f6*t)+offset)}')
   read v1 v2 v3 i1 i2 i3 <<<"$VALUES"
+  v1=$(maybe_null "$v1")
+  v2=$(maybe_null "$v2")
+  v3=$(maybe_null "$v3")
+  i1=$(maybe_null "$i1")
+  i2=$(maybe_null "$i2")
+  i3=$(maybe_null "$i3")
   JSON_PAYLOAD+="{\"n\":$n,\"v1\":$v1,\"v2\":$v2,\"v3\":$v3,\"i1\":$i1,\"i2\":$i2,\"i3\":$i3},"
 done
 JSON_PAYLOAD="${JSON_PAYLOAD%,}]}"
